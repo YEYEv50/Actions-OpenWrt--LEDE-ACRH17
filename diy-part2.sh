d6146ed7ce315bbdc1fe735927e78f9697feb6da
YEYEv50 <148872414+YEYEv50@users.noreply.github.com>
2024-04-11 23:06:48 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,6 +23,6 @@ rm -rf feeds/luci/themes/luci-theme-argon-mod
 rm -rf feeds/luci/applications/luci-app-argon-config
 #rm -rf feeds/packages/lang/golang
 
-git clone -b https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone -b https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 #git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
